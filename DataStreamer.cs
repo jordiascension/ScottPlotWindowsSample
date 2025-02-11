47484f44cf0e96aad2f08d4f0406e858aa51141b
jordiascension <jordiascension@enterpriseconsulting.es>
2025-02-11 10:26:06 +0100

    Added Draggable Axis Lines and implement it in DataStreamer Sample

--- DataStreamer.cs
+++ DataStreamer.cs
@@ -19,6 +19,8 @@ namespace ScottPlotSample
         public string Description => "Plots live streaming data as a fixed-width line plot, " +
             "shifting old data out as new data comes in.";
 
+        AxisLine? PlottableBeingDragged = null;
+
         readonly System.Windows.Forms.Timer AddNewDataTimer = new() { Interval = 10, Enabled = true };
         readonly System.Windows.Forms.Timer UpdatePlotTimer = new() { Interval = 50, Enabled = true };
 
@@ -37,6 +39,21 @@ namespace ScottPlotSample
             Streamer1 = formsPlot1.Plot.Add.DataStreamer(2000);
             Streamer2 = formsPlot1.Plot.Add.DataStreamer(2000);
             VLine = formsPlot1.Plot.Add.VerticalLine(0, 2, ScottPlot.Colors.Red);
+
+            var vl = formsPlot1.Plot.Add.VerticalLine(23);
+            vl.IsDraggable = true;
+            vl.Text = "VLine";
+
+            var hl = formsPlot1.Plot.Add.HorizontalLine(0.42);
+            hl.IsDraggable = true;
+            hl.Text = "HLine";
+
+            formsPlot1.Refresh();
+
+            // use events for custom mouse interactivity
+            formsPlot1.MouseDown += FormsPlot1_MouseDown;
+            formsPlot1.MouseUp += FormsPlot1_MouseUp;
+            formsPlot1.MouseMove += FormsPlot1_MouseMove;
 
             // disable mouse interaction by default
             //formsPlot1.UserInputProcessor.Disable();
@@ -129,6 +146,66 @@ namespace ScottPlotSample
                     Streamer2.ManageAxisLimits = false;
                 }
             };
+        }
+
+        private void FormsPlot1_MouseDown(object? sender, MouseEventArgs e)
+        {
+            var lineUnderMouse = GetLineUnderMouse(e.X, e.Y);
+            if (lineUnderMouse is not null)
+            {
+                PlottableBeingDragged = lineUnderMouse;
+                formsPlot1.UserInputProcessor.Disable(); // disable panning while dragging
+            }
+        }
+
+        private void FormsPlot1_MouseUp(object? sender, MouseEventArgs e)
+        {
+            PlottableBeingDragged = null;
+            formsPlot1.UserInputProcessor.Enable(); // enable panning again
+            formsPlot1.Refresh();
+        }
+
+        private void FormsPlot1_MouseMove(object? sender, MouseEventArgs e)
+        {
+            // this rectangle is the area around the mouse in coordinate units
+            CoordinateRect rect = formsPlot1.Plot.GetCoordinateRect(e.X, e.Y, radius: 10);
+
+            if (PlottableBeingDragged is null)
+            {
+                // set cursor based on what's beneath the plottable
+                var lineUnderMouse = GetLineUnderMouse(e.X, e.Y);
+                if (lineUnderMouse is null) Cursor = Cursors.Default;
+                else if (lineUnderMouse.IsDraggable && lineUnderMouse is VerticalLine) Cursor = Cursors.SizeWE;
+                else if (lineUnderMouse.IsDraggable && lineUnderMouse is HorizontalLine) Cursor = Cursors.SizeNS;
+            }
+            else
+            {
+                // update the position of the plottable being dragged
+                if (PlottableBeingDragged is HorizontalLine hl)
+                {
+                    hl.Y = rect.VerticalCenter;
+                    hl.Text = $"{hl.Y:0.00}";
+                }
+                else if (PlottableBeingDragged is VerticalLine vl)
+                {
+                    vl.X = rect.HorizontalCenter;
+                    vl.Text = $"{vl.X:0.00}";
+                }
+                formsPlot1.Refresh();
+            }
+        }
+
+        private AxisLine? GetLineUnderMouse(float x, float y)
+        {
+            CoordinateRect rect = formsPlot1.Plot.GetCoordinateRect(x, y, radius: 10);
+
+            foreach (AxisLine axLine in formsPlot1.Plot.GetPlottables<AxisLine>().Reverse())
+            {
+                if (axLine.IsUnderMouse(rect))
+                    return axLine;
+            }
+
+            return null;
         }
     }
 }
